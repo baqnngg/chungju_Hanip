fb5fdc8a831762afe27c4cbcbb99c4924b446248
baqnngg <il8696546@gmail.com>
2025-06-02 16:23:34 +0900

    6_2

--- flutter_project/lib/widgets/restaurant_card.dart
+++ flutter_project/lib/widgets/restaurant_card.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class RestaurantCard extends StatelessWidget {
   final String name;
   final String address;
+  final int reviewCount;   // 리뷰 수 변수 추가
   final VoidCallback? onTap;
 
   const RestaurantCard({
     required this.name,
     required this.address,
+    required this.reviewCount,  // 필수로 받기
     this.onTap,
     Key? key,
   }) : super(key: key);
@@ -35,6 +37,14 @@ class RestaurantCard extends StatelessWidget {
                     )),
                 const SizedBox(height: 4),
                 Text(address),
+                const SizedBox(height: 8),
+                Text(
+                  '리뷰 수: $reviewCount',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
               ],
             ),
           ),
